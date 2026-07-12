41c2480cfecf78c40bd9c8d24d987ec1684ec769
laxmimangaj5-Ak47 <laxmimangaj5@gmail.com>
2026-07-12 10:46:06 +0530

    project complete

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+const dns = require("dns");
+
+dns.setServers([
+    "8.8.8.8",
+    "8.8.4.4"
+]);
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const morgan = require('morgan');
+PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+// import routes file
+const authRoutes = require('./routes/authRoutes');
+const productRoutes = require('./routes/productRoutes');
+const cartRoutes = require('./routes/cartRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+
+//connect db
+connectDB();
+
+//golbal middleware
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+//routes
+
+app.use('/',authRoutes);
+app.use('/products',productRoutes);
+app.use('/cart',cartRoutes);
+app.use('/orders',orderRoutes);
+
+
+
+app.get('/',(req,res) =>{
+    res.send('KLE Ecommerce Backend is Running');
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is connected to the port ${PORT}`)
+})
